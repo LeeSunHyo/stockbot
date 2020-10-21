a6c066d4094d6c0b88ced407856e8cbca3662be9
SUNHYO <dltjsgy160@naver.com>
2020-10-21 17:10:46 +0900

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ client.once('ready', () => {
 
 
 // 여러분의 디스코드 토큰으로 디스코드에 로그인합니다
-client.login(`NzY4MDc3MTkxNzgyNTk2NjA4.X47NoA.x0d4koGa3KOQf-KqwHIsDzV2Uk8`);
+client.login(`NzY4MDc3MTkxNzgyNTk2NjA4.X47NoA.de01XNr4gA_O6mrKVUa_bsWOgsM`);
 
 
 client.on('message', message => {
